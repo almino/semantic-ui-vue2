38f250361cd525f17ce73749d9985d7ff54c2eb2
Almino Melo <almino@sobral.ufc.br>
2017-02-14 15:36:25 -0300

    Labels
    Ainda faltam muitas variações, mas já é compatível com .ui.labeled.input
    http://semantic-ui.com/elements/label.html

---
+++ elements/Label.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+    <div v-bind:class="['ui', getSizeClasses(), 'label']">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import Size from '../mixins/common/eight-sizes.js'
+
+export default {
+    mixins: [Size]
+}
+</script>
---
+++ mixins/common/eight-sizes.js
@@ -0,0 +1,40 @@
+import Constants from './constants.js';
+
+const sizes = [
+    Constants.massive,
+    Constants.huge,
+    Constants.big,
+    Constants.large,
+    Constants.medium,
+    Constants.small,
+    Constants.tiny,
+    Constants.mini,
+];
+
+export default {
+    props: {
+        size: {
+            type: [String, Number],
+            required: false,
+            // default: false,
+            validator(value) {
+                return value > 0 || value < 9 || sizes.indexOf(value) > -1;
+            }
+        },
+    },
+    methods: {
+        getSemanticSizes(value) {
+            if (typeof value === 'undefined') {
+                return sizes;
+            }
+
+            return sizes[value - 1];
+        },
+        isSemanticSize(value) {
+            return sizes.indexOf(value) > -1;
+        },
+        getSizeClasses() {
+            return isNaN(this.size) ? this.size : this.getSemanticSizes(this.size);
+        }
+    }
+}
