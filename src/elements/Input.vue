18ca6f1716fa017c7e12b6a17b9f4a0b27c59d9e
Almino <almino.melo@gmail.com>
2017-05-15 15:23:42 -0300

    Faltou um import

--- src/elements/Input.vue
+++ src/elements/Input.vue
@@ -67,6 +67,7 @@
 </template>
 
 <script>
+import Constants from '../mixins/commons/constants.js'
 import Input from '../mixins/commons/input.js'
 import Focus from '../mixins/commons/states/focus.js'
 import Loading from '../mixins/commons/states/loading.js'
@@ -117,7 +118,7 @@ export default {
     methods: {
         getIconClasses() {
             if (this.dIcon.value) {
-                return (this.dIcon.position == 'right' ? 'icon' : this.dIcon.position + ' icon')
+                return (this.dIcon.position == Constants.right ? Constants.icon : this.dIcon.position + ' ' + Constants.icon)
             }
 
             return false
