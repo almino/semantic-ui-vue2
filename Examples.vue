f2b965c6aa402772affcb22a4310777959fb8737
Almino <almino.melo@gmail.com>
2017-06-01 19:21:24 -0300

    Plural

--- Examples.vue
+++ Examples.vue
@@ -10,13 +10,8 @@
                 <ui-dropdown selection v-bind:items="alignments" v-model="grid.align"></ui-dropdown>
             </div>
         </div>
-        <ui-grid
-            v-bind:container="grid.container"
-            v-bind:equal-width="grid.equalWidth"
-            v-bind:column="grid.column"
-            v-bind:align="grid.align"
-            v-bind:float="grid.float"
-            v-bind:centered="grid.centered">
+        <ui-grid v-bind:container="grid.container" v-bind:equal-width="grid.equalWidth" v-bind:column="grid.column" v-bind:align="grid.align"
+            v-bind:float="grid.float" v-bind:centered="grid.centered">
             <ui-column v-for="i in columns" key="i">{{ i }}</ui-column>
         </ui-grid>
     </div>
@@ -24,20 +19,26 @@
 
 <script>
     import Constants from './src/mixins/commons/constants.js'
-    import { Mixin } from './index.js'
+    import {
+        Mixin
+    } from './index.js'
 
     export default {
         mixins: [Mixin],
         data() {
             return {
-                alignments: [
-                    Constants.bottom,
-                    Constants.center,
-                    Constants.left,
-                    Constants.middle,
-                    Constants.right,
-                    Constants.top,
-                ],
+                alignments: {
+                    horizontal: [
+                        Constants.center,
+                        Constants.left,
+                        Constants.right,
+                    ],
+                    vertical: [
+                        Constants.bottom,
+                        Constants.middle,
+                        Constants.top,
+                    ],
+                },
                 grid: {
                     container: false,
                     equalWidth: false,
@@ -50,7 +51,8 @@
         },
         computed: {
             columns() {
-                return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu lectus, porttitor sit amet erat congue.'.split(' ')
+                return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu lectus, porttitor sit amet erat congue.'
+                    .split(' ')
             }
         }
     }
@@ -58,43 +60,49 @@
 
 <style lang="less" scoped>
     /* Shows content box (not negative margins) */
+
     .grid {
-    position: relative;
+        position: relative;
     }
+
     .grid:before {
-    position: absolute;
-    top: 1rem;
-    left: 1rem;
-    background-color: #F0F0F0;
-    content: '';
-    width: calc(100% - 2rem);
-    height: calc(100% - 2rem);
-    box-shadow: 0px 0px 0px 1px #DDDDDD inset;
+        position: absolute;
+        top: 1rem;
+        left: 1rem;
+        background-color: #F0F0F0;
+        content: '';
+        width: calc(100% - 2rem);
+        height: calc(100% - 2rem);
+        box-shadow: 0px 0px 0px 1px #DDDDDD inset;
     }
+
     .ui.divided.grid:before,
     .celled.grid:before {
-    display: none;
+        display: none;
     }
+
     .ui.aligned .column:after {
-    // display: none !important;
+        // display: none !important;
     }
+
     .grid .column:not(.row):not(.grid):after {
-    background-color: rgba(86, 61, 124, .15);
-    box-shadow: 0px 0px 0px 1px rgba(86, 61, 124, 0.2) inset;
-    content: "";
-    display: block;
-    min-height: 50px;
+        background-color: rgba(86, 61, 124, .15);
+        box-shadow: 0px 0px 0px 1px rgba(86, 61, 124, 0.2) inset;
+        content: "";
+        display: block;
+        min-height: 50px;
     }
+
     @media only screen and (max-width: 768px) {
-    .stackable.grid:before {
-        width: 100%;
-        left: 0em;
-    }
+        .stackable.grid:before {
+            width: 100%;
+            left: 0em;
+        }
     }
 
     .ui {
         &.segment {
-            & > * {
+            &>* {
                 margin-left: 1em;
 
                 &:first-child {
@@ -102,9 +110,5 @@
                 }
             }
         }
-
     }
 </style>
-
-
-
