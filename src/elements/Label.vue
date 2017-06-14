0f8166fb7fc3c0fab8a1b748d00c3647985f74db
Unknown <unknown@example.com>
2017-06-14 14:23:01 -0300

    Um marcador pode ser qualquer elemento HTML

--- src/elements/Label.vue
+++ src/elements/Label.vue
@@ -1,13 +1,14 @@
 <template lang="html">
-    <div v-bind:class="['ui', getSizeClasses(), 'label']">
+    <component v-bind:is="as" v-bind:class="['ui', getSizeClasses(), 'label']">
         <slot></slot>
-    </div>
+    </component>
 </template>
 
 <script>
 import Size from '../mixins/commons/sizes/eight.js'
+import As from '../mixins/commons/props/as.js'
 
 export default {
-    mixins: [Size]
+    mixins: [As, Size]
 }
 </script>
---
+++ src/mixins/commons/props/as.js
@@ -0,0 +1,9 @@
+export default {
+    props: {
+        as: {
+            type: String,
+            required: false,
+            default: 'div',
+        },
+    }
+}
