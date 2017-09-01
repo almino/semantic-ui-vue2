4fa6a962ab0aeda209831140145cb7504cd7b967
Nicolas <nicolas.jouanin@gmail.com>
2017-09-01 22:44:10 +0200

    Allow field input to have icon

--- src/collections/form/Field.vue
+++ src/collections/form/Field.vue
@@ -6,61 +6,64 @@
             {{ label }}
             <slot name="label"></slot>
         </label>
-        <input
-            v-bind:id="id"
-            v-bind:accept="accept"
-            v-bind:alt="alt"
-            v-bind:autocomplete="autocomplete"
-            v-bind:autofocus="autofocus"
-            v-bind:checked="checked"
-            v-bind:dirname="dirname"
-            v-bind:disabled="disabled"
-            v-bind:form="form"
-            v-bind:formaction="formaction"
-            v-bind:formenctype="formenctype"
-            v-bind:formmethod="formmethod"
-            v-bind:formnovalidate="formnovalidate"
-            v-bind:formtarget="formtarget"
-            v-bind:list="list"
-            v-bind:max="max"
-            v-bind:maxlength="maxlength"
-            v-bind:min="min"
-            v-bind:multiple="multiple"
-            v-bind:name="name"
-            v-bind:pattern="pattern"
-            v-bind:placeholder="placeholder"
-            v-bind:readonly="readonly"
-            v-bind:required="required"
-            v-bind:src="src"
-            v-bind:step="step"
-            v-bind:type="type"
-            v-bind:value="value"
-            v-on:keydown="emitKeyDown"
-            v-on:keypress="emitKeyPress"
-            v-on:keyup="emitKeyUp"
-            v-on:mouseenter="emitMouseEnter"
-            v-on:mouseover="emitMouseOver"
-            v-on:mousemove="emitMouseMove"
-            v-on:mousedown="emitMouseDown"
-            v-on:mouseup="emitMouseUp"
-            v-on:click="emitClick"
-            v-on:dblclick="emitDoubleClick"
-            v-on:wheel="emitWheel"
-            v-on:mouseleave="emitMouseLeave"
-            v-on:mouseout="emitMouseOut"
-            v-on:pointerlockchange="emitPointerLockChange"
-            v-on:pointerlockerror="emitPointerLockError"
-            v-on:blur="emitBlur"
-            v-on:change="emitChange($event.target.value)"
-            v-on:contextmenu="emitContextMenu"
-            v-on:focus="emitFocus"
-            v-on:input="emitInput($event.target.value)"
-            v-on:invalid="emitInvalid"
-            v-on:reset="emitReset"
-            v-on:search="emitSearch"
-            v-on:select="emitSelect"
-            v-on:submit="emitSubmit"
-            ref="input" />
+        <div v-bind:class="[getIconClasses(), 'ui', 'input']">
+            <input
+                v-bind:id="id"
+                v-bind:accept="accept"
+                v-bind:alt="alt"
+                v-bind:autocomplete="autocomplete"
+                v-bind:autofocus="autofocus"
+                v-bind:checked="checked"
+                v-bind:dirname="dirname"
+                v-bind:disabled="disabled"
+                v-bind:form="form"
+                v-bind:formaction="formaction"
+                v-bind:formenctype="formenctype"
+                v-bind:formmethod="formmethod"
+                v-bind:formnovalidate="formnovalidate"
+                v-bind:formtarget="formtarget"
+                v-bind:list="list"
+                v-bind:max="max"
+                v-bind:maxlength="maxlength"
+                v-bind:min="min"
+                v-bind:multiple="multiple"
+                v-bind:name="name"
+                v-bind:pattern="pattern"
+                v-bind:placeholder="placeholder"
+                v-bind:readonly="readonly"
+                v-bind:required="required"
+                v-bind:src="src"
+                v-bind:step="step"
+                v-bind:type="type"
+                v-bind:value="value"
+                v-on:keydown="emitKeyDown"
+                v-on:keypress="emitKeyPress"
+                v-on:keyup="emitKeyUp"
+                v-on:mouseenter="emitMouseEnter"
+                v-on:mouseover="emitMouseOver"
+                v-on:mousemove="emitMouseMove"
+                v-on:mousedown="emitMouseDown"
+                v-on:mouseup="emitMouseUp"
+                v-on:click="emitClick"
+                v-on:dblclick="emitDoubleClick"
+                v-on:wheel="emitWheel"
+                v-on:mouseleave="emitMouseLeave"
+                v-on:mouseout="emitMouseOut"
+                v-on:pointerlockchange="emitPointerLockChange"
+                v-on:pointerlockerror="emitPointerLockError"
+                v-on:blur="emitBlur"
+                v-on:change="emitChange($event.target.value)"
+                v-on:contextmenu="emitContextMenu"
+                v-on:focus="emitFocus"
+                v-on:input="emitInput($event.target.value)"
+                v-on:invalid="emitInvalid"
+                v-on:reset="emitReset"
+                v-on:search="emitSearch"
+                v-on:select="emitSelect"
+                v-on:submit="emitSubmit"
+                ref="input" />
+            <i v-if="icon" v-bind:class="[icon, 'icon']"></i>
+        </div>
     </div>
 </template>
 
@@ -69,9 +72,10 @@ import Input from '../../mixins/commons/input/'
 import RandomProp from '../../mixins/commons/props/random.js'
 import Error from '../../mixins/commons/states/error.js'
 import Width from '../../mixins/commons/width.js'
+import Icon from '../../mixins/commons/icon.js'
 
 export default {
-    mixins: [Input, Error, Width],
+    mixins: [Input, Error, Width, Icon],
     props: {
         label: {
             type: String,
